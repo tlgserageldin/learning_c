f3bba0da5b70fbe4ab35e01eb691265cafc1c2b9
tlgserageldin <tserageldin@proton.me>
2023-07-11 13:28:00 +0300

    updated part1 and completed part2

--- aoc_2015_2.c
+++ aoc_2015_2.c
@@ -20,11 +20,27 @@ long calc_surface_area(const long l, const long w, const long h) {
 	return (2*l*w) + (2*l*h) + (2*w*h);
 }
 
+long calc_wrapping_ribbon(const long l, const long w, const long h) {
+	if (l >= w && l >= h) {
+		return 2*w+2*h;
+	}
+	else if (w >= l && w >= h) {
+		return 2*l+2*h;
+	}
+	else {
+		return 2*w+2*l;
+	}
+}
+
+long calc_bow(const long l, const long w, const long h) {
+	return l*w*h;
+}
+
 
 int main(void) {
 	long l, w, h;
-	long small_area, surface_area, paper, total_paper;
-	total_paper = 0;
+	long paper, ribbon;
+	paper = ribbon = 0;
 	FILE * f_input;
 	char line[SIZE];
 	f_input = fopen("aoc_2015_2_input.txt", "r");
@@ -32,12 +48,10 @@ int main(void) {
 		l = atol(strtok(line, "x"));
 		w = atol(strtok(NULL, "x"));
 		h = atol(strtok(NULL, "x"));
-		small_area = calc_smallest_side_area(l, w, h);
-		surface_area = calc_surface_area(l, w, h);
-		paper = small_area + surface_area;
+		paper += calc_surface_area(l, w, h) + calc_smallest_side_area(l, w, h);
+		ribbon += calc_wrapping_ribbon(l, w, h) + calc_bow(l, w, h);
 //		printf("l:%ld w:%ld h:%ld paper:%ld\n", l, w, h, paper);
-		total_paper += paper;
 	}
-	printf("total_paper = %ld\n", total_paper);
+	printf("total paper = %ld and total ribbon = %ld\n", paper, ribbon);
 	return 0;
 }
