80d63f67ba1ed7d7207c0edc6edf39759bb4f2ca
tlgserageldin <tserageldin@proton.me>
2023-07-13 15:27:55 +0300

    looked at problem 4 but above my paygrade... later

---
+++ aoc_2015/aoc_2015_4.c
@@ -0,0 +1,18 @@
+/* To do this, he needs to find MD5 hashes which, in hexadecimal, start with at least five zeroes.
+ * The input to the MD5 hash is some secret key (your puzzle input, given below) followed by a number in decimal.
+ * To mine AdventCoins, you must find Santa the lowest positive number (no leading zeroes: 1, 2, 3, ...) that produces such a hash.
+
+For example:
+
+    If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043 starts with five zeroes (000001dbbfa...), and it is the lowest such number to do so.
+    If your secret key is pqrstuv, the lowest number it combines with to make an MD5 hash starting with five zeroes is 1048970; that is, the MD5 hash of pqrstuv1048970 looks like 000006136ef....
+*/
+
+/*
+ * i want to implement md5 myself, that seems above my pay grade still, will come back to this...
+ */
+#include <stdio.h>
+
+int main(void) {
+	return 0;
+}
