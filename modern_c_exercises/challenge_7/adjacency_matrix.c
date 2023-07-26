6988433c7e4412d52ff05564ff435746fa06646b
tlgserageldin <tserageldin@proton.me>
2023-07-27 01:01:24 +0300

    starting challenege 7

--- modern_c_exercises/challenge_7/adjacency_matrix.c
+++ modern_c_exercises/challenge_7/adjacency_matrix.c
@@ -1,2 +1,12 @@
 /*
+The adjacency matrix of a graph G is a matrix A that holds a value true or false in element A[i][j] if there is an arc from node i to node j.
+    -- every row is a node on the tree
+    -- every element of that row is the other nodes
+At this point, can you use an adjacency matrix to conduct a breadth-first search in a graph G?
+Can you find connected components?
+Can you find a spanning tree?
 */
+
+int main(void) {
+    return 0;
+}
