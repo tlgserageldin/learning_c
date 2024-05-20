c6d4f0fde2895fb0c13165a4816ce17f4e6e21c8
tlgserageldin <tserageldin@protonmail.com>
2024-05-20 13:57:04 -0600

    extending original code for derivatives

--- modern_c_worked_examples/challenge_5_complex_derivatives/complex_derivatives.c
+++ modern_c_worked_examples/challenge_5_complex_derivatives/complex_derivatives.c
@@ -1,21 +1,31 @@
-/*Can you extend the derivative (challenge 2) to the complex domain: that is, functions that receive and return double complex values?*/
+/*Can you extend the derivative (challenge 2) to the complex domain: that is, functions that receive and return double complex values?
+  z = z0 + h = (x0 + h) + i*y0
+  F(z) = u(x, y) + i*v(x, y)
+  f(z) = (F(x0 + h + i*y0) - F(x0 + i*y0)) / h
+  as h -> 0i*/
+#include <math.h>
 #include <stdlib.h>
-#include <tgmath.h>
+#include <stdio.h>
 
 // numerical derivative of function F
-double complex f(double complex x);
+double f(double x);
 // original function which to derivate
-double complex F(double complex x);
+double F(double x);
 
 int main(int argc, char* argv[argc+1]) {
+    double result = f(2);
+    printf("expented result: %f", 0.354);
+    printf("result: %f", result);
     return EXIT_SUCCESS;
 }
 
-double complex F(double complex x) {
-    return sqrt(x);
+double F(double x) {
+    return sqrtf(x);
 }
 
-double complex f(double complex x) {
-    double complex h = .001 + I;
-    return (F(x+h) - F(x-h)) / (2*h);
+double f(double x) {
+    // will use the symmetrical difference quotient
+    // f(x+h) - f(x-h) / 2h
+    double h = 0.001; // if good enough for ti good enough for me
+    return (F(x+h)-F(x-h))/(2*h);
 }
